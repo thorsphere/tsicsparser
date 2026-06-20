f5573857018b4030f3f166802aaa487a2978247c
Thorsphere <99222474+thorsphere@users.noreply.github.com>
2026-06-20 19:13:07 +0000

    Implement initial calendar and event structures in icsparser.go

--- icsparser.go
+++ icsparser.go
@@ -2,3 +2,16 @@
 // All Rights Reserved. Use is governed with GNU Affero General Public License v3.0
 // that can be found in the LICENSE file.
 package tsicsparser
+
+import "time"
+
+// Calendar represents a calendar with events.
+type Calendar struct {
+	Events []Event
+}
+
+// Event represents a calendar event with a summary and start time.
+type Event struct {
+	Summary  string
+	Start    time.Time
+}
