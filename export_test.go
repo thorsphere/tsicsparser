efff61380f1f2a9a43892cd0a90f738397a6e090
Thorsphere <99222474+thorsphere@users.noreply.github.com>
2026-07-18 21:07:35 +0000

    update copyright headers to FSL-1.1-ALv2 and export internal parsers for testing
    Update all source file copyright headers to reference the Functional Source
    License v1.1 (FSL-1.1-ALv2). Export parseWeekday,
    parseByDay, and nthWeekday in export_test.go so they can be tested from the
    external test package.

--- export_test.go
+++ export_test.go
@@ -1,10 +1,13 @@
 // Copyright (c) 2026 thorsphere.
-// All Rights Reserved. Use is governed with GNU Affero General Public License v3.0
-// that can be found in the LICENSE file.
+// All Rights Reserved. Use is governed by the Functional Source License v1.1
+// (FSL-1.1-ALv2) that can be found in the LICENSE file.
 package tsicsparser
 
 // Export internal functions for external test package.
 var (
 	ParseTimezone = parseTimezone
 	ParseProdId   = parseProdID
+	ParseWeekday  = parseWeekday
+	ParseByDay    = parseByDay
+	NthWeekday    = nthWeekday
 )
